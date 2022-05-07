ae4148ada9d0fc0ead167b93910957be091b3812
ArelyGonz <99548393+ArelyGonz@users.noreply.github.com>
2022-05-06 23:08:29 -0500

    Add files via upload

---
+++ Scrips/ifpowershell.ps1
@@ -0,0 +1,7 @@
+﻿$serviceName = 'wuauserv'
+$serviceInfo = Get-service -Name $serviceName
+$serviceName
+if ($serviceInfo.Status -eq "Running")
+{
+Write-Host $serviceName "Esta corriendo"
+}
---
+++ Scrips/switchpowershell.ps1
@@ -0,0 +1,16 @@
+﻿$serviceName = Read-Host -Prompt 'Ingresa el nombre del servicio'
+$opc = Read-Host -Prompt 'Que deseas hacer? [1] Ver status [2] Iniciar [3] Detener'
+switch ($opc){
+1{
+Get-Service -Name $serviceName
+break
+} 2 {
+Start-Service -Name $serviceName
+break
+} 3 {
+Stop-Service -Name $serviceName 
+break
+} default {
+Write-Host "Opcion no valida"
+}
+}
